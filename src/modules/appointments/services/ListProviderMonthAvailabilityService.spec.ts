0356c8c026f53956fa45ac95c58a858db1610b5d
Yuri Burk <yuriburk@gmail.com>
2020-05-31 20:00:58 -0300

    feature: Creating ListProverMonthAvailability service

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,55 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderMonthAvailability: ListProviderMonthAvailabilityService;
+
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderMonthAvailability = new ListProviderMonthAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the month availability from provider', async () => {
+    const promises = [];
+    for (let i = 0; i <= 10; i += 1) {
+      promises.push(
+        fakeAppointmentsRepository.create({
+          provider_id: 'provider-id',
+          date: new Date(2020, 4, 21, i, 0, 0),
+        }),
+      );
+    }
+
+    await Promise.all(promises);
+
+    const availability = await listProviderMonthAvailability.execute({
+      provider_id: 'provider-id',
+      year: 2020,
+      month: 5,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        {
+          day: 19,
+          available: true,
+        },
+        {
+          day: 20,
+          available: true,
+        },
+        {
+          day: 21,
+          available: false,
+        },
+        {
+          day: 22,
+          available: true,
+        },
+      ]),
+    );
+  });
+});
