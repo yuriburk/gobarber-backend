47c65688e702f4bae00650f6e252b47b8269b622
Yuri Burk <yuriburk@gmail.com>
2020-06-02 12:14:13 -0300

    feature: Created listproviderappointments service

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the appointments on a specific day', async () => {
+    const firstAppointment = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: 'user-id',
+      date: new Date(2020, 4, 21, 13, 0, 0),
+    });
+
+    const secondAppointment = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: 'user-id',
+      date: new Date(2020, 4, 21, 14, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider-id',
+      day: 21,
+      year: 2020,
+      month: 5,
+    });
+
+    expect(appointments).toEqual([firstAppointment, secondAppointment]);
+  });
+});
